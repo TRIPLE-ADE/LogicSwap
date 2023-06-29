6f93ed8a85af1aa6ca0390c24d2364781a5a7ffd
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2023-06-29 06:46:54 +0100

    Update hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
 import { section } from "../style"
 const Hero = () => {
   return (
-    <section className="bg-neutral-200 pt-10">
+    <section className="pt-16 bg-neutral-200">
        <div className={`${section.flexCenter} ${section.padding} sm:items-center text-center min-h-[85vh] sm:w-4/5 md:w-[65%] m-auto`}>
-            <p className="text-lg text-primary font-semibold">Transforming Ideas into Digital Success</p>
-            <h1 className="text-3xl sm:text-5xl font-bold font-quicksand my-2">Expert IT Solutions for Your Business Needs!</h1>
+            <p className="text-lg font-semibold text-primary">Transforming Ideas into Digital Success</p>
+            <h1 className="my-2 text-3xl font-bold sm:text-5xl font-quicksand">Expert IT Solutions for Your Business Needs!</h1>
             <p className="text-neutral-2">We provide comprehensive web programming, cyber IT consulting, and networking services to help your business succeed.</p>
-            <button className="bg-secondary hover:bg-dark-green px-5 py-3 mt-5 rounded-3xl text-white font-semibold"><a href="mailto:">Contact Us</a></button>
+            <button className="px-5 py-3 mt-5 font-semibold text-white bg-secondary hover:bg-dark-green rounded-3xl"><a href="mailto:">Contact Us</a></button>
        </div>
     </section>
   )
