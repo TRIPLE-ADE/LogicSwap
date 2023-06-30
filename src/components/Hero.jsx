3d1207bb948b03e86f0ea4f472e2101197ddca07
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2023-06-30 07:43:22 +0100

    Update footer component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
             <p className="text-lg font-semibold text-primary">Transforming Ideas into Digital Success</p>
             <h1 className="my-2 text-3xl font-bold sm:text-5xl font-quicksand">Expert IT Solutions for Your Business Needs!</h1>
             <p className="text-neutral-2">We provide comprehensive web programming, cyber IT consulting, and networking services to help your business succeed.</p>
-            <button className="px-5 py-3 mt-5 font-semibold text-white bg-secondary hover:bg-dark-green rounded-3xl"><a href="mailto:">Contact Us</a></button>
+            <button className={`${section.button}`}><a href="mailto:">Contact Us</a></button>
        </div>
     </section>
   )
