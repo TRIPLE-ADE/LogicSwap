851aa5ad2a0c4780b253095630c52196ba588842
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2023-07-02 18:15:07 +0100

    Update hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,11 @@ import { section } from "../style"
 import { hero } from "../assets"
 const Hero = () => {
   return (
-    <section className="bg-neutral-200" id="home" style={{backgroundImage: `url(${hero})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
+    <section className="bg-neutral-200" id="home" style={{backgroundImage: `url(${hero})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center'}}>
        <div className={`${section.flexCenter} ${section.padding} sm:items-center justify-center text-center min-h-[100vh] sm:w-4/5 md:w-[70%] m-auto`}>
             <p className="text-lg font-semibold text-primary">Transforming Ideas into Digital Success</p>
             <h1 className="my-2 text-3xl font-bold sm:text-5xl font-quicksand">Expert IT Solutions for Your Business Needs!</h1>
-            <p className="text-neutral-2">We provide comprehensive web programming, cyber security, IT consulting, and networking services to help your business succeed.</p>
+            <p className="font-medium text-neutral-2">We provide comprehensive Web Programming, Cyber Security, IT Consulting, and Networking Services to help your Business succeed.</p>
             <button className="px-5 py-3 mt-5 font-semibold text-white bg-secondary hover:bg-dark-green rounded-3xl"><a href="mailto:">Contact Us</a></button>
        </div>
     </section>
