83be871a23025d020e12986ea0b6a24646040c31
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2023-07-02 18:35:06 +0100

    Update testimonials component

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -4,29 +4,29 @@ import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
 
 const Testimonials = () => {
   return (
-    <section className={`${section.padding} text-center bg-white`} id="testimonial">
+    <section className={`${section.padding} text-center bg-white scroll-m-20`} id="testimonial">
       <h2 className={`${section.sectionHeading}`}>Testimonials</h2>
       <p className={`${section.sectionSubHeading} mb-10`}>
         Hear from our satisfied customers and clients
       </p>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
         {TestimonialsData.map((item, index) => (
           <div key={index} className="p-4 border rounded">
-            <div className="mb-4 flex gap-3">
+            <div className="flex gap-3 mb-4">
               <img
                 src={item.image}
                 alt={item.name}
-                className="w-10 h-10 rounded-full object-cover"
+                className="object-cover w-10 h-10 rounded-full"
               />
               <div>
-                <p className="text-gray-600 font-semibold">{item.name}</p>
+                <p className="font-semibold text-gray-600">{item.name}</p>
                 <p className="text-sm text-gray-500">{item.company}</p>
               </div>
             </div>
             <div className="flex">
                 <FaQuoteLeft  className="text-lg" />
                 <p className="flex-1">{item.message}</p>
-                <FaQuoteRight className="text-lg self-end" />
+                <FaQuoteRight className="self-end text-lg" />
             </div>
           </div>
         ))}
