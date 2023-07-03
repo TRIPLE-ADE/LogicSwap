800218199472399ea47bb22e791a00aaf61ef84e
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2023-07-03 18:56:49 +0100

    update about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,20 +8,19 @@ const About = () => {
       {AboutSection.map((about, index) => (
         <div key={about.id} className={`${index !== 0 ? 'py-5' : 'pb-5' }`}>
           {index === AboutSection.length - 1 ? (
-          <div>
-              <p className={`${section.sectionSubHeading}`}>{about.heading}</p>
-              <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 mt-7">
-                {about.text.map((list, index) => (
-                  <div key={index} className={`${section.serviceCard}`}>
-                    {list.icon}
-                    <h3 className="font-bold">{list.heading}</h3>
-                    <p>{list.content}</p>
-                  </div>
-                ))}
-              </div>
-          </div>
-          ) : (
-           
+            <div>
+                <p className={`${section.sectionSubHeading}`}>{about.heading}</p>
+                <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 mt-7">
+                  {about.text.map((list, index) => (
+                    <div key={index} className={`${section.serviceCard}`}>
+                      {list.icon}
+                      <h3 className="font-bold">{list.heading}</h3>
+                      <p>{list.content}</p>
+                    </div>
+                  ))}
+                </div>
+            </div>
+          ) : ( 
             <div className="flex flex-wrap items-center justify-center">
               <div className="w-full p-4 sm:w-1/2">
                 <h3 className="mb-4 text-2xl font-semibold">{about.heading}</h3>
@@ -33,8 +32,8 @@ const About = () => {
                 <img src={about.img} alt={about.heading} className="mx-auto mb-4 md:w-[80%] " />
               </div>
             </div>
-          )}
-        </div>
+            )}
+          </div>
       ))}
     </section>
   );
