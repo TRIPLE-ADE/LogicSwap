10ed68e4be6af409e36553c40cefd25593adec92
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2023-07-03 05:02:53 +0100

    Update service component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -7,7 +7,7 @@ const Services = () => {
         <h2 className={`${section.sectionHeading}`}>Our Services</h2>
         <p className={`${section.sectionSubHeading}`}>We Offer different kind of Tech Solution</p>
         <p  className="max-w-[37rem] m-auto text-justify sm:text-center">At LogicSwap, we understand that businesses have unique challenges and requirements. which is why we offer a wide array of tech solutions designed to cater to diverse needs. Explore our range of services:</p>
-        <div className="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-10 mt-7">
+        <div className="grid grid-cols-1 gap-10 md-grid-cols-2 lg:grid-cols-3 mt-7">
             {
                 ServiceCard.map((service) => (
                     <div className={`${section.serviceCard}`} key={service.id}>
