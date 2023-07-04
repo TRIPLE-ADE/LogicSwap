7e99922731cdd814a9e49cfc443dbceb8e897dff
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2023-07-04 13:19:34 +0100

    update companyLogo component

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -5,13 +5,13 @@ const CompanyLogos = () => {
   return (
     <section className={`${section.padding} text-center bg-white`}>
         <h2 className={`${section.sectionSubHeading}`}>Trusted by</h2>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 mt-7 mx-auto w-[50%]">
+        <div className="grid gap-4 grid-cols-3 mt-7 mx-auto w-[80%] sm:w-[60%] md:w-[50%]">
           {CompanyLogosData.map((logo, index) => (
             <img
               key={index}
               src={logo.logo}
               alt={`Company Logo ${index}`}
-              className="m-auto w-[70px]"
+              className="m-auto w-[50%] md:w-[70px]"
             />
           ))}
         </div>
