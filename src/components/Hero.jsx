284ba0f676f5aad28d5030655ca688dc309870cd
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2023-07-04 12:41:47 +0100

    update hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
        <div className={`${section.flexCenter} ${section.padding} sm:items-center justify-center text-center min-h-[100vh] sm:w-4/5 md:w-[70%] m-auto`}>
             <p className="text-lg font-semibold text-primary">Transforming Ideas into Digital Success</p>
             <h1 className="my-2 text-3xl font-bold sm:text-5xl font-quicksand">Expert IT Solutions for Your Business Needs!</h1>
-            <p className="font-medium text-neutral-2">We provide comprehensive Web Programming, Cyber Security, IT Consulting, and Networking Services to help your Business succeed.</p>
+            <p className="font-medium text-neutral-2">We provide comprehensive Web Programming, Cyber Security, IT Consulting, Networking Services and Cloud Computing to help your Business succeed.</p>
             <button className="px-5 py-3 mt-5 font-semibold text-white bg-secondary hover:bg-dark-green rounded-3xl"><a href="mailto:logicswaptech@gmail.com">Contact Us</a></button>
        </div>
     </section>
